ccd130775338480479bd0bd7a12f095e5414c954
Masaki Miyauchi <masaki@miyauchi.me>
2019-04-12 14:01:02 +0900

    JSのLint環境の設定

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    // 'eslint:recommended',
+    'airbnb',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
